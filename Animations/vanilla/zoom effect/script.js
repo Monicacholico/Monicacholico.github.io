d0b7e5b8be032d6987d3c3ca0d4282f3805485a3
Monicacholico <monicachol@hotmail.com>
2021-11-28 21:02:52 -0800

    updating image size

--- Animations/vanilla/zoom effect/script.js
+++ Animations/vanilla/zoom effect/script.js
@@ -1,19 +1,43 @@
-const {body} = document;
-let zoomActivated = false;
+// const {body} = document;
 
-window.addEventListener('keydown', e => {
-    if(e.key === 'z') {
-        zoomActivated = !zoomActivated;
-    }
+let zoomActivated = false;
+let goBack = document.querySelector('.return')
+goBack.addEventListener('click', () => {
+    zoomActivated = !zoomActivated;
+    image.style.transform = 'none';
 });
 
-window.addEventListener('mousemove', e => {
-    const { clientX: x, clientY: y} = e;
-    if(zoomActivated) {
-        body.style.transform = 'scale(2)';
-        body.style.transformOrigin = `${x}px ${y}px`;
-    } else {
-        body.style.transform = 'none';
-    }
+const image = document.querySelector('.content-wrapper');
+console.log(image.clientWidth);
+console.log(image.clientHeight);
+// window.addEventListener('keydown', e => {
+//     if(e.key === 'z') {
+//         zoomActivated = !zoomActivated;
+//     }
+// });
 
+// window.addEventListener('mousemove', e => {
+//     const { clientX: x, clientY: y} = e;
+//     if(zoomActivated) {
+//         body.style.transform = 'scale(2)';
+//         body.style.transformOrigin = `${x}px ${y}px`;
+//     } else {
+//         body.style.transform = 'none';
+//     }
+
+// })
+
+const point1 = document.getElementById('point-1');
+console.log(point1);
+
+point1.addEventListener('click', () => {
+    // image.clientHeight
+    zoomActivated = true;
+    console.log('youre clicking here');
+    if(zoomActivated) {
+        image.style.transform = 'scale(1.5)';
+        image.style.transformOrigin = `${image.clientWidth}px - 250px ${image.clientHeight}px`;
+    } else {
+        image.style.transform = 'none';
+    }
 })
